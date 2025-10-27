f3c54b746d62fecc411acd07ffac1440c8370e17
Saul Dominguez <dguezs@gmail.com>
2025-10-27 16:26:52 -0600

    feat: adding service layer to separate logic

--- controllers/estudiantes.js
+++ controllers/estudiantes.js
@@ -1,10 +1,13 @@
 const { response } = require('express');
-const Estudiante = require('../models/estudiantes');
+const {listar, obtenerPorMatricula, crear, actualizarPorMatricula, eliminarPorMatricula, actualizarEdadPorMatricula} = require('../services/estudiantes');
 
 const getEstudiantes = async (req, res = response) => {
     try {
-        const estudiantes = await Estudiante.find();
-        res.json(estudiantes);
+        const { page = 1, limit = 50 } = req.query;
+        const skip = (Number(page) - 1) * Number(limit);
+        console.log("skip: "+skip);
+        const {items, total} = await listar(skip, limit);
+        res.json({ total, page: Number(page), limit: Number(limit), items });
     } catch (error) {
         res.status(500).json({ msg: 'Error al obtener estudiantes', error });
     }
@@ -13,7 +16,7 @@ const getEstudiantes = async (req, res = response) => {
 const getEstudiante = async (req, res = response) => {
     const { matricula } = req.params;
     try {
-        const estudiante = await Estudiante.findOne({ matricula: parseInt(matricula) });
+        const estudiante = await obtenerPorMatricula(parseInt(matricula));
         if (!estudiante) return res.status(404).json({ msg: 'Estudiante no encontrado' });
         res.json(estudiante);
     } catch (error) {
@@ -22,10 +25,9 @@ const getEstudiante = async (req, res = response) => {
 };
 
 const addEstudiante = async (req, res = response) => {
-    const { matricula, nombre, carrera, edad } = req.body;
+    const { matricula, nombre, carrera = 'LIS', edad =20} = req.body;
     try {
-        const nuevoEstudiante = new Estudiante({ matricula, nombre, carrera, edad });
-        await nuevoEstudiante.save();
+        const nuevoEstudiante = await crear({ matricula, nombre, carrera, edad });
         res.status(201).json({
             msg: `El estudiante ${nombre} ha sido creado`,
             estudiante: nuevoEstudiante
@@ -46,11 +48,7 @@ const updateEstudiante = async (req, res = response) => {
     const { matricula } = req.params;
     const { nombre, carrera, edad } = req.body;
     try {
-        const updatedEstudiante = await Estudiante.findOneAndUpdate(
-            { matricula: parseInt(matricula) },
-            { nombre, carrera, edad },
-            { new: true, runValidators: true}
-        );
+        const updatedEstudiante = await actualizarPorMatricula(parseInt(matricula), { nombre, carrera, edad });
         if (!updatedEstudiante) {
             return res.status(404).json({ msg: 'Estudiante no encontrado' });
         }
@@ -70,11 +68,7 @@ const updateEdadEstudiante = async (req, res = response) => {
     const { matricula } = req.params;
     const { edad } = req.body;
     try {
-        const updatedEstudiante = await Estudiante.findOneAndUpdate(
-            { matricula: parseInt(matricula) },
-            { edad },
-            { new: true, runValidators: true }
-        );
+        const updatedEstudiante = await actualizarEdadPorMatricula(parseInt(matricula), parseInt(edad))
         if (!updatedEstudiante) {
             return res.status(404).json({ msg: 'Estudiante no encontrado' });
         }
@@ -93,7 +87,7 @@ const deleteEstudiante = async (req, res = response) => {
         return res.status(400).json({ msg: 'Matrícula no válida' });
     }
     try {
-        const deletedEstudiante = await Estudiante.findOneAndDelete({ matricula: matricula });
+        const deletedEstudiante = await eliminarPorMatricula(parseInt(matricula));
         if (!deletedEstudiante) {
             return res.status(404).json({ msg: 'Estudiante no encontrado' });
         }
